3cfeb2bf5a6eb9fd23c1a7517772b24f1e57e71a
Emma Borkent <emma_borkent@msn.com>
2019-05-02 18:27:15 +0200

    after turning amounts keeps adding to list, only not displayed in list, but can be seen when printing

--- app/src/main/java/com/ishiki/mizuwodrinkwater/Controller/MainActivity.kt
+++ app/src/main/java/com/ishiki/mizuwodrinkwater/Controller/MainActivity.kt
@@ -16,15 +16,6 @@ class MainActivity : AppCompatActivity() {
     private var dailyTotal = 0
     private val waterAmount = 250
 
-    override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState)
-
-        outState?.run {
-            putInt(EXTRA_DAILY, dailyTotal)
-            putStringArray(EXTRA_LIST, drinksToday.toTypedArray())
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,8 +24,8 @@ class MainActivity : AppCompatActivity() {
             dailyTotalText.text = addWater().toString()
             drinksToday.add(0, "250 ml")
 
-//            // Print to check
-//            println(drinksToday.toString())
+            // Print to check
+            println(drinksToday.toString())
 //            for (i in drinksToday) {
 //                println(i)
 //            }
@@ -49,6 +40,19 @@ class MainActivity : AppCompatActivity() {
 
         adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, drinksToday)
         drinksTodayList.adapter = adapter
+
+        if (savedInstanceState != null) {
+
+            savedInstanceState.run {
+                dailyTotal = getInt(EXTRA_DAILY)
+                dailyTotalText.text = dailyTotal.toString()
+
+                val list = getStringArray(EXTRA_LIST)
+                if (list != null) {
+                    drinksToday = list.toMutableList<String>()
+                }
+            }
+        }
     }
 
     private fun addWater(): Int {
@@ -61,24 +65,32 @@ class MainActivity : AppCompatActivity() {
         return dailyTotal
     }
 
-    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
-        super.onRestoreInstanceState(savedInstanceState)
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
 
-        savedInstanceState?.run {
-            dailyTotal = getInt(EXTRA_DAILY)
-            dailyTotalText.text = dailyTotal.toString()
-
-            val list = getStringArray(EXTRA_LIST)
-
-            if (list != null) {
-                drinksToday = list.toMutableList()
-
-                // Print to check
-                for (i in list) {
-                    println(i)
-                }
-            }
+        outState?.run {
+            putInt(EXTRA_DAILY, dailyTotal)
+            putStringArray(EXTRA_LIST, drinksToday.toTypedArray())
         }
     }
 
+//    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+//        super.onRestoreInstanceState(savedInstanceState)
+//
+//        savedInstanceState?.run {
+//            dailyTotal = getInt(EXTRA_DAILY)
+//            dailyTotalText.text = dailyTotal.toString()
+//
+//            val list = getStringArray(EXTRA_LIST)
+//
+//            if (list != null) {
+//                drinksToday = list.toMutableList<String>()
+//
+//                // Print to check
+//                for (i in list) {
+//                    println(i)
+//                }
+//            }
+//        }
+//    }
 }
