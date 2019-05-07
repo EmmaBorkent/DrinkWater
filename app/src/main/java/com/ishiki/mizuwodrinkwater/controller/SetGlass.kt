73816d37ba302a792cd0e6a224366e351f1da035
Emma Borkent <emma_borkent@msn.com>
2019-05-07 21:01:11 +0200

    started building the activity_set_glass

---
+++ app/src/main/java/com/ishiki/mizuwodrinkwater/controller/SetGlass.kt
@@ -0,0 +1,13 @@
+package com.ishiki.mizuwodrinkwater.controller
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ishiki.mizuwodrinkwater.R
+
+class SetGlass : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_glass)
+    }
+}
