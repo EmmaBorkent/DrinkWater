870df2a0ebe454a5bd8b655c52fb5453a2821595
Emma Borkent <emma_borkent@msn.com>
2019-05-09 11:25:30 +0200

    added glasses list to dataservices file. Found out that when pressing back, the list state is saved, but the drinksTodayTotal is not.

--- app/src/main/java/com/ishiki/mizuwodrinkwater/services/DataService.kt
+++ app/src/main/java/com/ishiki/mizuwodrinkwater/services/DataService.kt
@@ -1,6 +1,14 @@
 package com.ishiki.mizuwodrinkwater.services
 
+import com.ishiki.mizuwodrinkwater.model.Drinks
+
 // Here we want all the arrays that hold data
 object DataService {
+
     var drinksToday: MutableList<String> = mutableListOf()
+
+    val glasses = listOf<Drinks>(
+        Drinks("glass", "water01", "250", "ml"),
+        Drinks("bottle", "water02", "500", "ml")
+    )
 }
