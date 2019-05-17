1669ff3b159a5502d216a136cf3c8c81834fecd1
Emma Borkent <emma_borkent@msn.com>
2019-05-17 09:47:21 +0200

    Drinks RecyclerView is now clickable and returns the right information

--- app/src/main/java/com/ishiki/mizuwodrinkwater/controller/SetGlassActivity.kt
+++ app/src/main/java/com/ishiki/mizuwodrinkwater/controller/SetGlassActivity.kt
@@ -30,10 +30,8 @@ class SetGlassActivity : AppCompatActivity() {
 
         dailyTotal = intent.getIntExtra(EXTRA_DAILY, dailyTotal)
         currentGlass = intent.getParcelableExtra(EXTRA_CURRENT)
-
-        adapter = GlassesAdapter(this, DataService.drinks) {
+        adapter = GlassesAdapter(this, DataService.drinks) { drink ->
             // Here goes the code that you want to happen when you click on it
-            drink ->
             println(drink.glass)
 
             when (drink.glass) {
@@ -48,6 +46,24 @@ class SetGlassActivity : AppCompatActivity() {
             setGlassIntent.putExtra(EXTRA_SET, currentGlass)
             startActivity(setGlassIntent)
         }
+
+//        adapter = GlassesAdapter(this, DataService.drinks) {
+//
+//            drink ->
+//            println(drink.glass)
+//
+//            when (drink.glass) {
+//                "glass" -> currentGlass = glass
+//                "bottle" -> currentGlass = bottle
+//                "juice" -> currentGlass = juice
+//                else -> Toast.makeText(this, "No Glass Was Selected", Toast.LENGTH_LONG).show()
+//            }
+//
+//            val setGlassIntent = Intent(this, MainActivity::class.java)
+//            setGlassIntent.putExtra(EXTRA_DAILY, dailyTotal)
+//            setGlassIntent.putExtra(EXTRA_SET, currentGlass)
+//            startActivity(setGlassIntent)
+//        }
         val layoutManager = GridLayoutManager(this, 2)
         setGlassListView?.layoutManager = layoutManager
         setGlassListView?.adapter = adapter
@@ -56,39 +72,39 @@ class SetGlassActivity : AppCompatActivity() {
         println("Currently using a ${currentGlass.glass}")
     }
 
-    fun setGlass250(@Suppress("UNUSED_PARAMETER") view: View) {
-        currentGlass = glass
-
-        // Print to check
-        println("Changed to ${currentGlass.glass}")
-
-        val glass250Intent = Intent(this, MainActivity::class.java)
-        glass250Intent.putExtra(EXTRA_DAILY, dailyTotal)
-        glass250Intent.putExtra(EXTRA_SET, currentGlass)
-        startActivity(glass250Intent)
-    }
-
-    fun setGlass500(@Suppress("UNUSED_PARAMETER") view: View) {
-        currentGlass = bottle
-
-        // Print to check
-        println("Changed to ${currentGlass.glass}")
-
-        val glass500Intent = Intent(this, MainActivity::class.java)
-        glass500Intent.putExtra(EXTRA_DAILY, dailyTotal)
-        glass500Intent.putExtra(EXTRA_SET, currentGlass)
-        startActivity(glass500Intent)
-    }
-
-    fun setGlassJuice(@Suppress("UNUSED_PARAMETER") view: View) {
-        currentGlass = juice
-
-        // Print to check
-        println("Changed to ${currentGlass.glass}")
-
-        val glassJuiceIntent = Intent(this, MainActivity::class.java)
-        glassJuiceIntent.putExtra(EXTRA_DAILY, dailyTotal)
-        glassJuiceIntent.putExtra(EXTRA_SET, currentGlass)
-        startActivity(glassJuiceIntent)
-    }
+//    fun setGlass250(@Suppress("UNUSED_PARAMETER") view: View) {
+//        currentGlass = glass
+//
+//        // Print to check
+//        println("Changed to ${currentGlass.glass}")
+//
+//        val glass250Intent = Intent(this, MainActivity::class.java)
+//        glass250Intent.putExtra(EXTRA_DAILY, dailyTotal)
+//        glass250Intent.putExtra(EXTRA_SET, currentGlass)
+//        startActivity(glass250Intent)
+//    }
+//
+//    fun setGlass500(@Suppress("UNUSED_PARAMETER") view: View) {
+//        currentGlass = bottle
+//
+//        // Print to check
+//        println("Changed to ${currentGlass.glass}")
+//
+//        val glass500Intent = Intent(this, MainActivity::class.java)
+//        glass500Intent.putExtra(EXTRA_DAILY, dailyTotal)
+//        glass500Intent.putExtra(EXTRA_SET, currentGlass)
+//        startActivity(glass500Intent)
+//    }
+//
+//    fun setGlassJuice(@Suppress("UNUSED_PARAMETER") view: View) {
+//        currentGlass = juice
+//
+//        // Print to check
+//        println("Changed to ${currentGlass.glass}")
+//
+//        val glassJuiceIntent = Intent(this, MainActivity::class.java)
+//        glassJuiceIntent.putExtra(EXTRA_DAILY, dailyTotal)
+//        glassJuiceIntent.putExtra(EXTRA_SET, currentGlass)
+//        startActivity(glassJuiceIntent)
+//    }
 }
