1604260182879a89fe9f58561cf4ba8aa568f66f
Emma Borkent <emma_borkent@msn.com>
2019-05-17 17:31:41 +0200

    added functions to skip through images in create glass function

--- app/src/main/java/com/ishiki/mizuwodrinkwater/controller/CustomGlass.kt
+++ app/src/main/java/com/ishiki/mizuwodrinkwater/controller/CustomGlass.kt
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.ishiki.mizuwodrinkwater.R
+import kotlinx.android.synthetic.main.activity_custom_glass.*
+import kotlin.random.Random
 
 class CustomGlass : AppCompatActivity() {
 
@@ -16,5 +18,38 @@ class CustomGlass : AppCompatActivity() {
     fun setCustomGlass(@Suppress("UNUSED_PARAMETER") view: View) {
         val setCustomGlassIntent = Intent(this, SetGlassActivity::class.java)
         startActivity(setCustomGlassIntent)
+    }
+
+    var number = 1
+    fun nextImage(view: View) {
+        if (number < 10) {
+            number += 1
+        } else {
+            number = 1
+        }
+
+        val name = "water0$number"
+
+        val resourceId = resources.getIdentifier(name, "drawable", packageName)
+        customGlassImage.setImageResource(resourceId)
+
+        // Print to check
+        println(number)
+    }
+
+    fun prevImage(view: View) {
+        if (number > 1) {
+            number -= 1
+        } else {
+            number = 10
+        }
+
+        val name = "water0$number"
+
+        val resourceId = resources.getIdentifier(name, "drawable", packageName)
+        customGlassImage.setImageResource(resourceId)
+
+        // Print to check
+        println(number)
     }
 }
