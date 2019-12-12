9d54f9ff906db1cb53b9c649a373ce5e61314f71
Emma Borkent <emma_borkent@msn.com>
2019-12-12 08:55:13 +0100

    Added a Scroll View and Time Picker in the reminders fragment.

--- app/src/main/java/com/ishiki/mizuwodrinkwater/fragments/RemindersFragment.kt
+++ app/src/main/java/com/ishiki/mizuwodrinkwater/fragments/RemindersFragment.kt
@@ -1,5 +1,7 @@
 package com.ishiki.mizuwodrinkwater.fragments
 
+import android.app.TimePickerDialog
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,9 +10,16 @@ import androidx.fragment.app.Fragment
 import com.ishiki.mizuwodrinkwater.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_reminders.*
+import java.text.SimpleDateFormat
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.*
 
 
 class RemindersFragment : Fragment() {
+
+    private var sharedPreferences: SharedPreferences? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +33,16 @@ class RemindersFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         activity?.glassesAddButton?.hide()
+
+        sharedPreferences = activity?.getSharedPreferences(
+            getString(R.string.shared_preferences_file), 0
+        )
+
+        if (sharedPreferences != null) {
+
+        } else {
+
+        }
 
         reminders_back_arrow_left.setOnClickListener {
             activity?.glassesAddButton?.show()
@@ -40,6 +59,7 @@ class RemindersFragment : Fragment() {
                     reminders_when_container.visibility = View.VISIBLE
                     reminders_time_container.visibility = View.VISIBLE
                     reminders_how_often_container.visibility = View.VISIBLE
+
                 }
                 false -> {
                     reminders_when_container.visibility = View.INVISIBLE
@@ -49,6 +69,52 @@ class RemindersFragment : Fragment() {
             }
         }
 
-    }
+        reminders_time_from_select.text = "8:00 AM"
+        reminders_time_to_select.text = "4:00 PM"
 
+        val timeFrom = LocalTime.parse(reminders_time_from_select.text,
+            DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT))
+        val timeTo = LocalTime.parse(reminders_time_to_select.text,
+            DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT))
+
+        val timeFormat = SimpleDateFormat("hh:mm")
+
+        reminders_time_from_select.setOnClickListener {
+            // Create time picker
+            val hour = timeFrom.hour
+            val minute = timeFrom.minute
+            val timePicker = TimePickerDialog(activity,
+                TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                    // If we change the time in time picker we'll get the callback in OnTimeSetListener
+                    // Do something with the time chosen by the user
+                    val selectedTime = Calendar.getInstance()
+                    selectedTime.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                    selectedTime.set(Calendar.MINUTE, minute)
+
+                    reminders_time_from_select.text = timeFormat.format(selectedTime.time)
+
+                }, hour, minute, true)
+
+            timePicker.show()
+        }
+
+        reminders_time_to_select.setOnClickListener {
+            val hour = timeTo.hour
+            val minute = timeTo.minute
+            val timePicker = TimePickerDialog(activity,
+                TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
+                    // If we change the time in time picker we'll get the callback in OnTimeSetListener
+                    // Do something with the time chosen by the user
+                    val selectedTime = Calendar.getInstance()
+                    selectedTime.set(Calendar.HOUR_OF_DAY, hourOfDay)
+                    selectedTime.set(Calendar.MINUTE, minute)
+
+                    reminders_time_from_select.text = timeFormat.format(selectedTime.time)
+
+                }, hour, minute, true)
+
+            timePicker.show()
+        }
+
+    }
 }
---
+++ app/src/main/java/com/ishiki/mizuwodrinkwater/model/Reminders.kt
@@ -0,0 +1,4 @@
+package com.ishiki.mizuwodrinkwater.model
+
+class Reminders {
+}
